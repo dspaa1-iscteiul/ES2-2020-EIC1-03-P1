8f6d80abbe3b27efbae4f86bd37005e36e245d8e
dspaa1-iscteiul <dspaa1@iscte-iul.pt>
2020-03-24 23:57:58 +0000

    Teste

--- ES2-2020-EIC1-03-P1/src/main/java/calculator/Calculator.java
+++ ES2-2020-EIC1-03-P1/src/main/java/calculator/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
 	 * @param args - ""
 	 */
 	public static void main(String[] args) {
-		System.out.println("Calculadora ES2");
+		System.out.println("Calculadora-ES2");
 	}
 	
 	/**
